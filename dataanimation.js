1dbc3bd4502d6372984a467e592cdb0e8d9b68e9
laurenhsueh <lhsueh@usc.edu>
2025-05-08 12:55:11 -0700

    9 steps!

--- dataanimation.js
+++ dataanimation.js
@@ -39,8 +39,8 @@ $(document).ready(function() {
     $("#exit").on("click", function() {
         $("#frWalkthrough").fadeOut();
         $("#frWalkthrough > *").hide();
-
-        $(".chartsection").children().show();
+        $(".chartsection").children().not(".dstep").show();
+        $("#wkdata1, #wkdata2, #wkdata3, #wkdata4, #wkdata5, #wkdata6, #wkdata7, #wkdata8, #wkdata9").show();
 
         $("#exit").hide();
         $("#walkthrough").show();
@@ -48,6 +48,7 @@ $(document).ready(function() {
     });
 
     // Step transitions
+    // NEXT buttons
     $("#step1next").on("click", function() {
         showStep("#wkdesc1, #wkdata1", "#wkdesc2, #wkdata2");
     });
@@ -67,6 +68,52 @@ $(document).ready(function() {
     $("#step5next").on("click", function() {
         showStep("#wkdesc5, #wkdata5", "#wkdesc6, #wkdata6");
     });
+
+    $("#step6next").on("click", function() {
+        showStep("#wkdesc6, #wkdata6", "#wkdesc7, #wkdata7");
+    });
+
+    $("#step7next").on("click", function() {
+        showStep("#wkdesc7, #wkdata7", "#wkdesc8, #wkdata8");
+    });
+
+    $("#step8next").on("click", function() {
+        showStep("#wkdesc8, #wkdata8", "#wkdesc9, #wkdata9");
+    });
+
+    // PREVIOUS buttons
+    $("#step2prev").on("click", function() {
+        showStep("#wkdesc2, #wkdata2", "#wkdesc1, #wkdata1");
+    });
+
+    $("#step3prev").on("click", function() {
+        showStep("#wkdesc3, #wkdata3", "#wkdesc2, #wkdata2");
+    });
+
+    $("#step4prev").on("click", function() {
+        showStep("#wkdesc4, #wkdata4", "#wkdesc3, #wkdata3");
+    });
+
+    $("#step5prev").on("click", function() {
+        showStep("#wkdesc5, #wkdata5", "#wkdesc4, #wkdata4");
+    });
+
+    $("#step6prev").on("click", function() {
+        showStep("#wkdesc6, #wkdata6", "#wkdesc5, #wkdata5");
+    });
+
+    $("#step7prev").on("click", function() {
+        showStep("#wkdesc7, #wkdata7", "#wkdesc6, #wkdata6");
+    });
+
+    $("#step8prev").on("click", function() {
+        showStep("#wkdesc8, #wkdata8", "#wkdesc7, #wkdata7");
+    });
+
+    $("#step9prev").on("click", function() {
+        showStep("#wkdesc9, #wkdata9", "#wkdesc8, #wkdata8");
+    });
+
 
     const hash = window.location.hash;
 
